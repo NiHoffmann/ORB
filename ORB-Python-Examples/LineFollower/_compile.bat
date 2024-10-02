935c003e4b3dd6cd25d242a896b9141fd67debad
Nils Hoffmann <82971538+NiHoffmann@users.noreply.github.com>
2024-10-02 10:53:01 +0200

    add line follower example first draft

---
+++ ORB-Python-Examples/LineFollower/_compile.bat
@@ -0,0 +1,2 @@
+set ORB_PATH=C:\Users\nils9\Desktop\Bachelorarbeit\ORB\
+python %ORB_PATH%\ORB-Python\tools\compile.py line_follower.py
---
+++ ORB-Python-Examples/LineFollower/line_follower.py
@@ -0,0 +1,67 @@
+from devices import sensor, motor
+import time
+
+class Mode():
+    LIGHT = 0
+    AMBIENT = 1
+    COLOR = 2      
+
+class Color():
+    NONE = 0
+    BLACK = 1
+    BLUE = 2
+    GREEN = 3
+    YELLOW = 4
+    RED = 5
+    WHITE = 6
+    BROWN = 7
+
+class EV3ColorSensor:
+    def __init__(self, _port):   
+        self.me = sensor(port = _port, type = sensor.UART,mode = Mode.COLOR)    
+
+    def get(self):
+        return self.me.get()["values"][0] & 0xFFFF
+
+colorLeft = EV3ColorSensor(sensor.S1)
+colorRight = EV3ColorSensor(sensor.S2)
+motorLeft = motor(port = motor.M1)
+motorRight = motor(port = motor.M2)
+motorLeft.set(mode = motor.SPEED_MODE)
+motorRight.set(mode = motor.SPEED_MODE)
+
+speedLeft =  25.0
+speedRight = 25.0
+
+def max(l,r):
+    if(l > r):
+        return l
+    return r
+
+def min(l,r):
+    if(l < r):
+        return l
+    return r
+
+last = time.getTime()
+current = last
+deltaTime = 0
+gain = 0.00001
+
+while True:
+    current = time.getTime()
+    deltaTime = last - current
+    last = current
+
+    if colorLeft.get() == Color.BLACK:
+        speedLeft = min(speedLeft + (gain * deltaTime), 100)
+    else:
+        speedLeft = max(speedLeft - (gain * deltaTime), 0)
+
+    if colorRight.get() == Color.BLACK:
+        speedRight = min(speedRight + (gain * deltaTime), 100)
+    else:
+        speedRight = max(speedRight - (gain * deltaTime), 0)
+
+    motorLeft.set(speed = int(speedLeft))
+    motorRight.set(speed = int(speedRight))
